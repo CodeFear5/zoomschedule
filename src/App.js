d8528ce4b2aca8b0a0fbd5403e9c95fb5110c09a
unknown <nageshbc55@gmail.com>
2024-06-22 19:59:59 +0530

    Removed box.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Box from './Box'
-import Schedule from './shedule'
-import Confirmation  from './confirmation';
-import Upcomingmeeting from './upcomingmeeting'
-import Activities from './activities'
+import Schedule from './shedule';
+import Confirmation from './confirmation';
+import Upcomingmeeting from './upcomingmeeting';
+import Activities from './activities';
 import Navbar from './navbar';
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<div>Home</div>} />
           <Route path="/schedule" element={<Schedule />} />
           <Route path="/confirmation" element={<Confirmation />} />
           <Route path="/upcoming" element={<Upcomingmeeting />} />
           <Route path="/activities" element={<Activities />} />
-
-
-
-          
+          <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </div>
     </Router>
